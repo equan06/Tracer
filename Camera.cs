9022d70bac12822030cd72b3142aa921b3233808
equan <equan06@gmail.com>
2021-04-04 11:43:52 -0700

    sampling pixels and diffuse materials

---
+++ Camera.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Numerics;
+
+namespace Tracer
+{
+    class Camera
+    {
+        private Vector3 horizontal;
+        private Vector3 vertical;
+        private Vector3 origin;
+        private Vector3 lowerLeftCorner;
+
+        public Camera()
+        {
+            const float AspectRatio = 16f / 9;
+
+            // Dimensions of the viewport
+            // This is basically the plane that we send our rays to
+            float viewportHeight = 2.0f;
+            float viewportWidth = viewportHeight * AspectRatio;
+            float focalLength = 1.0f;
+
+            origin = new Vector3(0, 0, 0);
+            horizontal = new Vector3(viewportWidth, 0, 0); // x 
+            vertical = new Vector3(0, viewportHeight, 0); // y 
+            // Create the corner of the viewport
+            lowerLeftCorner = origin - horizontal / 2 - vertical / 2 - new Vector3(0, 0, focalLength);
+        }
+
+        public Ray GetRay(float u, float v)
+        {
+            return new Ray(origin, lowerLeftCorner + u * horizontal + v * vertical - origin);
+        }
+    }
+}
--- Tracer.cs
+++ Tracer.cs
@@ -4,74 +4,81 @@ using System.Numerics;
 
 namespace Tracer
 {
-
     class Tracer
     {
         public const float infinity = int.MaxValue;
         public const float pi = (float)Math.PI;
+        public Random random = new Random();
 
         public float DegreesToRadians(float deg)
         {
             return deg * pi / 180;
         }
 
-        static void Main(string[] args)
+        public float RandUnit()
         {
-            const float AspectRatio = 16f / 9;
-            const int ImageWidth = 400;
-            const int ImageHeight = (int)(ImageWidth / AspectRatio);
+            return (float)random.NextDouble();
+        }
 
-            // Dimensions of the viewport
-            // This is basically the plane that we send our rays to
-            float viewportHeight = 2.0f;
-            float viewportWidth = viewportHeight * AspectRatio;
-            float focalLength = 1.0f;
+        public float RandFloat(float a, float b)
+        {
+            return a + (b - a) * (float)random.NextDouble();
+        }
 
-            Vector3 origin = new Vector3(0, 0, 0);
-            Vector3 horizontal = new Vector3(viewportWidth, 0, 0); // x 
-            Vector3 vertical = new Vector3(0, viewportHeight, 0); // y 
-            // Create the corner of the viewport
-            Vector3 lowerLeftCorner = origin - horizontal / 2 - vertical / 2 - new Vector3(0, 0, focalLength);
+        public Vector3 RandUnitVec()
+        {
+            return new Vector3(RandUnit(), RandUnit(), RandUnit());
+        }
 
-            HittableList world = new HittableList();
-            world.Add(new Sphere(new Vector3(0, 0, -1), 0.5f));
-            world.Add(new Sphere(new Vector3(0, -100.5f, -1), 100f));
+        public Vector3 RandFloatVec(float a, float b)
+        {
+            return new Vector3(RandFloat(a, b), RandFloat(a, b), RandFloat(a, b));
+        }
 
-            Console.WriteLine($"P3\n{ImageWidth} {ImageHeight}\n255");
+        public Vector3 RandVecInUnitSphere()
+        {
+            // Note: while you could do this, the distribution will be different than the below sampling scheme.
+            // This samples within a unit cube, and then normalizes (to be on the sphere)
+            //return Vector3.Normalize(RandFloatVec(-1, 1));
 
-            for (int j = ImageHeight - 1; j >= 0; j--)
+            // This samples within a unit sphere and then normalizes
+            while (true)
             {
-                Console.Error.WriteLine("\rScanlines remaining: " + j);
-                for (int i = 0; i < ImageWidth; i++)
-                {
-                    float u = (float)i / (ImageWidth - 1);
-                    float v = (float)j / (ImageHeight - 1);
-                    // Draw a ray from the origin to the viewport
-                    Ray r = new Ray(origin, lowerLeftCorner + u * horizontal + v * vertical - origin);
-                    // Compute the color at the intersection between the ray and the viewport
-                    WriteLineColor(RayColor(r, world));
-                }
+                Vector3 p = RandFloatVec(-1, 1);
+                if (p.LengthSquared() >= 1) continue;
+                return Vector3.Normalize(p);
             }
+        }
 
-            Console.Error.WriteLine("Done");
+        public float Clamp(float x, float min, float max)
+        {
+            return x < min ? min : (x > max ? max : x);
+        }
+
+        public Vector3 ClampVector(Vector3 v, float min, float max)
+        {
+            return new Vector3(Clamp(v.X, min, max), Clamp(v.Y, min, max), Clamp(v.Z, min, max));
         }
 
         /// <summary>
         /// Assume that v has values in [0, 1]. Writes an RGB value scaled by v. 
         /// </summary>
         /// <param name="v"></param>
-        public static void WriteLineColor(Vector3 v)
+        public void WriteLineColor(Vector3 v)
         {
             Console.WriteLine($"{(int)(255.999 * v.X)} {(int)(255.999 * v.Y)} {(int)(255.999 * v.Z)}");
         }
 
-        public static Vector3 RayColor(Ray r, HittableList world)
+        public Vector3 RayColor(Ray r, HittableList world, int depth)
         {
-            HitRecord rec = world.Hit(r, 0, infinity);
+            if (depth <= 0) return new Vector3(0, 0, 0);
+            // Note: using .001 instead of 0 removes rays that are close to t=0 as a result of floating point approximations
+            HitRecord rec = world.Hit(r, 0.001f, infinity);
             if (rec.isHit)
             {
-                // Calculate the surface normal of the closest object, and scale to [0,1]
-                return 0.5f * (rec.normal + new Vector3(1, 1, 1));
+                Vector3 target = rec.normal + RandVecInUnitSphere();
+                // Calculate the surface normal of the closest object, and send it in a random direction starting from the intersection point 
+                return 0.5f * RayColor(new Ray(rec.point, target), world, depth - 1);
             }
 
             Vector3 unitDirection = Vector3.Normalize(r.Direction);
@@ -81,21 +88,43 @@ namespace Tracer
             return (1 - t) * new Vector3(1, 1, 1) + t * new Vector3(0.5f, 0.7f, 1.0f);
         }
 
-
-        public static float HitSphere(Vector3 center, float radius, Ray r)
+        public void CreateImage()
         {
-            // Some simplifications applied (instead of using b, use b/2)
-            Vector3 rayToCenter = r.Origin - center;
-            float a = Vector3.Dot(r.Direction, r.Direction);
-            float halfb = Vector3.Dot(r.Direction, rayToCenter);
-            float c = Vector3.Dot(rayToCenter, rayToCenter) - radius * radius;
-            float discriminant = (halfb * halfb - 4 * a * c);
-            if (discriminant < 0)
-                return -1;
-            else
+            const float AspectRatio = 16f / 9;
+            const int ImageWidth = 400;
+            const int ImageHeight = (int)(ImageWidth / AspectRatio);
+            const int samplesPerPixel = 100;
+            const int maxDepth = 50;
+
+            Camera cam = new Camera();
+            HittableList world = new HittableList();
+            world.Add(new Sphere(new Vector3(0, 0, -1), 0.5f));
+            world.Add(new Sphere(new Vector3(0, -100.5f, -1), 100f));
+
+            Console.WriteLine($"P3\n{ImageWidth} {ImageHeight}\n255");
+            for (int j = ImageHeight - 1; j >= 0; j--)
             {
-                return (-halfb - (float)Math.Sqrt(discriminant)) / a;
+                Console.Error.WriteLine("\rScanlines remaining: " + j);
+                for (int i = 0; i < ImageWidth; i++)
+                {
+                    // Sample the pixel color by averaging over nearby sampled rays
+                    Vector3 pixelColor = new Vector3(0, 0, 0);
+                    for (int s = 0; s < samplesPerPixel; s++)
+                    {
+                        float u = (float)(i + RandUnit()) / (ImageWidth - 1);
+                        float v = (float)(j + RandUnit()) / (ImageHeight - 1);
+                        pixelColor += RayColor(cam.GetRay(u, v), world, maxDepth);
+                    }
+                    WriteLineColor(ClampVector(Vector3.SquareRoot(pixelColor / samplesPerPixel), 0, 0.999f));
+                }
             }
+            Console.Error.WriteLine("Done");
+        }
+
+        static void Main(string[] args)
+        {
+            Tracer t = new Tracer();
+            t.CreateImage();
         }
     }
 }
